480987955658200b600d6901b147b13e17a30c71
Tyler Hoff <tylehoff@umich.edu>
2017-12-14 11:46:27 -0500

    added example files

---
+++ config_example.py
@@ -0,0 +1,5 @@
+db_name = ''
+db_user = ''
+db_password = ''
+
+# Fill in the above fields with the relevant information for your database
---
+++ image_path_example.py
@@ -0,0 +1,1 @@
+image_shape = '' #Put the name of your desired image file here, it must be in the same folder
